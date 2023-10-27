83f52b283c238e78f487db0aa3aabb2fa22dbdf4
jwolf-git <jwolf-git@users.noreply.github.com>
2023-10-26 22:11:45 -0400

    Lots of days to add

---
+++ Intermediate/Day20/scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+#Global variables
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 270)
+        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER, SUCKA", align=ALIGNMENT, font=FONT)
